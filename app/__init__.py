01b7792cac11be1d07d9d22f4628a36d475d9db6
mwaz <amby.mwaz@gmail.com>
2017-11-22 22:37:55 +0300

    [CHORE] refactor to only hold endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
-from flask import request, jsonify, abort, make_response
+
 
 # local import
 from instance.config import app_config
@@ -11,6 +11,7 @@ db = SQLAlchemy()
 
 def make_app(config_name):
     from app.models import Categories
+    from app.classes import categories
 
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config['testing'])
@@ -18,81 +19,7 @@ def make_app(config_name):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
-    @app.route('/categories/', methods=['POST'])
-    def categories_post():
-        """"Method to add a new category to the endpoint"""
-        print("ok")
-        if request.method == "POST":
-            category_name = str(request.data.get('category_name', ''))
-            if category_name:
-                category = Categories(category_name=category_name)
-                category.save()
-                response = jsonify({
-                                    'id': category.id,
-                                    'category_name': category.category_name,
-                                    'date_created': category.date_created,
-                                    'date_modified': category.date_modified
-                                })
-                response.status_code = 201
-                return response
-            else:
-                response = {'message':category_name}
-                response = jsonify(response)
-                response.status_code = 401
-                return response
-
-    @app.route('/categories/', methods=['GET'])
-    def categories_get():
-        """"Method to get all categories"""
-        if request.method == "GET":
-            categories = Categories.get_all_user_categories()
-            results = []
-            for category in categories:
-                category_object = {
-                                    'id': category.id,
-                                    'category_name': category.category_name,
-                                    'date_created': category.date_created,
-                                    'date_modified': category.date_modified
-                                    }
-                results.append(category_object)
-            response = jsonify(results)
-            response.status_code = 200
-            return response
-
-    @app.route('/categories/<int:id>', methods=['GET', 'PUT', 'DELETE'])
-    def categories_manipulation(id, **kwargs):
-        # retrieve a category using it's ID
-        category = Categories.query.filter_by(id=id).first()
-        if not category:
-            # Raise an HTTPException with a 404 not found status code
-            abort(404)
-
-        if request.method == 'DELETE':
-            category.delete_categories()
-            return {
-                       "message": "category{} deleted successfully".format(category.id)
-                   }, 200
-
-        elif request.method == 'PUT':
-            category_name = str(request.data.get('category_name', ''))
-            category.category_name = category_name
-            category.save()
-            response = jsonify({
-                'id': category.id,
-                'category_name': category.category_name,
-                'date_created': category.date_created,
-                'date_modified': category.date_modified
-            })
-            response.status_code = 200
-            return response
-        else:
-            # GET
-            response = jsonify({
-                'id': category.id,
-                'category_name': category.category_name,
-                'date_created': category.date_created,
-                'date_modified': category.date_modified
-            })
-            response.status_code = 200
-            return response
+    from app.classes.categories import category_view_post,category_manipulation
+    app.add_url_rule('/flask_api/v1/categories/', view_func=category_view_post)
+    app.add_url_rule('/flask_api/v1/categories/<int:id>', view_func=category_manipulation)
     return app
