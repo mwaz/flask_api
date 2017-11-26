c79710988e022f046933e94c4da0840dddc2a450
mwaz <amby.mwaz@gmail.com>
2017-11-26 17:49:56 +0300

    [TEST] add test to check successful user login

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -12,6 +12,9 @@ class TestAuth(unittest.TestCase):
         self.user_details = {'email':'someone@gmail.com',
                              'password':'testing_p@ssword'
                              }
+        self.unathorized_user_details = {'email': 'uuser@unauthorized.com',
+                                         'password': 'none_provided'
+                                    }
 
         with self.app.app_context():
             db.session.close()
@@ -19,18 +22,32 @@ class TestAuth(unittest.TestCase):
             db.create_all()
 
     def testResisterUser(self):
-        user_register = self.client.post('/flask_api/v1/auth/register', data = self.user_details)
+        """"Method to test a successful registration"""
+        user_register = self.client().post('/flask_api/v1/auth/register', data = self.user_details)
         result = json.loads(user_register.data.decode())
         self.assertEqual(result['message'], "Successfully registered")
         self.assertEqual(user_register.status_code, 201)
 
     def test_double_registration(self):
-        user_register = self.client.post('/flask_api/v1/auth/register', data=self.user_details)
+        """"Method to test a user who is already registered """
+        user_register = self.client().post('/flask_api/v1/auth/register', data=self.user_details)
         self.assertEqual(user_register.status_code, 201)
-        double_user_registration = self.client.post('/flask_api/v1/auth/register', data = self.user_details)
+        double_user_registration = self.client().post('/flask_api/v1/auth/register', data=self.user_details)
         self.assertEqual(double_user_registration.status_code, 202)
         result = json.loads(double_user_registration.data.decode())
-        self.assertEqual(result['message'], "User Exists, Login")
+        self.assertEqual(result['message'], "User Exists, Kindly Login")
+
+    def test_user_login(self):
+        """"Method to test successful user login"""
+        user_register = self.client().post('/flask_api/v1/auth/register', data = self.user_details)
+        self.assertEqual(user_register.status_code, 201)
+        user_login = self.client().post('/flask_api/v1/auth/login', data = self.user_details)
+        result = json.loads(user_login.data.decode())
+        self.assertEqual(result['message'], 'Successful Login')
+        self.assertEqual(user_login.status_code, 200)
+        self.assertTrue(result['granted_token'])
+
+    
 
 
 
