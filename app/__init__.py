6f842f70e1d2b0818570dd49044a76a99e672830
mwaz <amby.mwaz@gmail.com>
2017-12-12 23:12:22 +0300

    [FEATURE] add category_search endpoint and a universal base_urlfor API access

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from instance.config import app_config
 
 # initialize sql-alchemy
 db = SQLAlchemy()
-
+base_url = '/yummy_api/v1'
 
 def make_app(config_name):
     from app.models import Categories
@@ -19,16 +19,17 @@ def make_app(config_name):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
-    from app.classes.categories import category_view_post,category_manipulation
-    app.add_url_rule('/yummy_api/v1/categories/', view_func=category_view_post)
-    app.add_url_rule('/yummy_api/v1/categories/<int:id>', view_func=category_manipulation)
+    from app.classes.categories import category_view_post,category_manipulation,category_view_search
+    app.add_url_rule(base_url + '/categories/', view_func=category_view_post)
+    app.add_url_rule(base_url + '/categories/<int:id>', view_func=category_manipulation)
+    app.add_url_rule(base_url + '/categories/search', view_func=category_view_search)
 
     from app.classes.recipes import recipe_post_get_view, recipe_manipulation_view
-    app.add_url_rule('/yummy_api/v1/categories/<int:id>/recipes/', view_func=recipe_post_get_view)
-    app.add_url_rule('/yummy_api/v1/categories/<int:id>/recipes/<int:recipe_id>', view_func=recipe_manipulation_view)
+    app.add_url_rule(base_url + '/categories/<int:id>/recipes/', view_func=recipe_post_get_view)
+    app.add_url_rule(base_url + '/categories/<int:id>/recipes/<int:recipe_id>', view_func=recipe_manipulation_view)
 
     from app.auth.authentication import user_registration_view,user_login_view
-    app.add_url_rule('/yummy_api/v1/auth/register', view_func=user_registration_view, methods=['POST'])
-    app.add_url_rule('/yummy_api/v1/auth/login', view_func=user_login_view, methods=['POST'])
+    app.add_url_rule(base_url + '/auth/register', view_func=user_registration_view, methods=['POST'])
+    app.add_url_rule(base_url + '/auth/login', view_func=user_login_view, methods=['POST'])
 
     return app
