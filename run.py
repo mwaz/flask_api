e9d7b1e027163c72bc335341979e15583aeb72bc
mwaz <amby.mwaz@gmail.com>
2018-01-02 10:43:23 +0300

    [FIX] remove ps scaling in Procfile

--- run.py
+++ run.py
@@ -3,15 +3,15 @@ from flask import redirect
 from app import make_app
 config_name = os.getenv('APP_SETTINGS') # config_name = "development"
 
-app = make_app(config_name)
-port = int(os.environ.get('PORT', 5000))
-host = "127.0.0.1"
-
 @app.route('/')
 def Rundocs():
     """method to run app documentation
     """
     return redirect('/apidocs/')
 
+
+app = make_app(config_name)
+port = 5000
+
 if __name__ == '__main__':
-    app.run(host=host, port=port, debug=True)
+    app.run(port=port)
