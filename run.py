f286e8da8747352c5ecfb7cf39e4203a90142bd0
mwaz <amby.mwaz@gmail.com>
2018-01-02 10:47:01 +0300

    [FIX] add app import in run.py

--- run.py
+++ run.py
@@ -3,15 +3,14 @@ from flask import redirect
 from app import make_app
 config_name = os.getenv('APP_SETTINGS') # config_name = "development"
 
+app = make_app(config_name)
+port = 5000
+
 @app.route('/')
 def Rundocs():
     """method to run app documentation
     """
     return redirect('/apidocs/')
-
-
-app = make_app(config_name)
-port = 5000
 
 if __name__ == '__main__':
     app.run(port=port)
