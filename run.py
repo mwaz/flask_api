63b6557dbd53fe96a5a7a24438c5bc720676b377
mwaz <amby.mwaz@gmail.com>
2018-01-02 10:33:21 +0300

    [FIX] Add port to run app in Heroku

--- run.py
+++ run.py
@@ -1,9 +1,17 @@
 import os
-
+from flask import redirect
 from app import make_app
 config_name = os.getenv('APP_SETTINGS') # config_name = "development"
 
 app = make_app(config_name)
+port = int(os.environ.get('PORT', 5000))
+host = "127.0.0.1"
+
+@app.route('/')
+def Rundocs():
+    """method to run app documentation
+    """
+    return redirect('/apidocs/')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host=host, port=port, debug=True)
